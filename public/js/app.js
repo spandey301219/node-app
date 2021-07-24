64e5f3ed3400efafa0760700ac18241d2142e018
sachin <sachin.kumar.pandey@sap.com>
2021-07-24 17:06:19 +0530

    heroku-setup

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ form.addEventListener('submit', (e) => {
     const searchTermValue = searchTerm.value;
     message1.textContent = 'loading....';
     message2.textContent = ''
-    fetch('http://localhost:3000/weather?address=' + searchTermValue).then((res)=> {
+    fetch('/weather?address=' + searchTermValue).then((res)=> {
     res.json().then((data) => {
         if (data) {
             message1.textContent = data;
